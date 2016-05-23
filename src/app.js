fa631d3ab22012145ef79edb39f51afa56f179a4
Nicolas Hahn <nicolas@stonespring.org>
2016-05-23 16:10:00 -0700

    add quotePage page

--- src/app.js
+++ src/app.js
@@ -17,32 +17,13 @@ var main = new UI.Card({
   bodyColor: '#9a0036' // Hex colors
 });
 
-main.show();
-/*
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+var quotePage = new UI.Card({
+	title: 'Random Quote',
+	body: body_obj.quote + '\n -' + body_obj.author,
+	scrollable: true
 });
-*/
+
+main.show();
 
 function getQuote(e) {
   var properties = { cat:'movies' }
@@ -55,70 +36,16 @@ function getQuote(e) {
     if(err) {console.log(err); return; }
     console.log('get response: '+JSON.stringify(res)); 
 		var body_obj = JSON.parse(body);
-    var quotePage = new UI.Card({
-      title: 'Random Quote',
-      body: body_obj.quote + '\n -' + body_obj.author,
-      scrollable: true
-    });
+		quotePage.show();
   });
-  /*
-  unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies")
-  .header("X-Mashape-Key", "d3efyiXfa1mshirwLZ34ztyor1JVp11jpGxjsnuIyAs4n8SHp5")
-  .header("Content-Type", "application/x-www-form-urlencoded")
-  .header("Accept", "application/json")
-  .end(function (result) {
-    console.log(result.status, result.headers, result.body);
-    var quotePage = new UI.Card({
-      title: 'Random Quote',
-      body: result.quote + '\n -' + result.author,
-      scrollable: true
-    });
-    quotePage.show();
-  });
-  */
-  /*
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-  */
 }
 
 main.on('click', 'select', getQuote);
 main.on('click', 'up', getQuote);
 main.on('click', 'down', getQuote);
 
-main.on('click', 'back', function(e) {
+quotePage('click', 'select', getQuote);
+quotePage.on('click', 'back', function(e) {
   main.show();
 });
 
