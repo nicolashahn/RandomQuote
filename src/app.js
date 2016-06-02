fd049d10100cdf382bfbba2c64e9bff36f2972ff
Nicolas Hahn <nicolas@stonespring.org>
2016-06-02 10:37:15 -0700

    it works!

--- src/app.js
+++ src/app.js
@@ -7,45 +7,35 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Random Quote',
-  // icon: 'images/menu_icon.png',
-  // subtitle: ...
-  body: 'Press any button to get a new quote.'
-  // subtitleColor: 'indigo', // Named colors
-  // bodyColor: '#9a0036' // Hex colors
+var quotePage = new UI.Card({
+  title: '',
+  body: 'Getting quote...',
+  scrollable: true,
+  style: 'small'
 });
 
-main.show();
-
 function getQuote(e) {
-  
-  var url = 'andruxnet-random-famous-quotes.p.mashape.com/?cat=movies';
+  console.log('getQuote() called');
+  var url = 'https://andruxnet-random-famous-quotes.p.mashape.com';
   var headers = {
     "X-Mashape-Key": "d3efyiXfa1mshirwLZ34ztyor1JVp11jpGxjsnuIyAs4n8SHp5",
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json"
   };
   ajax({url:url, headers:headers}, function(data) {
-    console.log(data);
-    var quotePage = new UI.Card({
-      title: 'Random Quote',
-      body: data.quote + '\n -' + data.author,
-      scrollable: true
-    });
+    var data_obj = JSON.parse(data);
+    console.log(data_obj.quote + ' -' + data_obj.author);
+    quotePage.title(data_obj.author);
+    quotePage.body(data_obj.quote);
     quotePage.show();
   });
-  
 }
 
-main.on('click', 'select', getQuote);
-main.on('click', 'up', getQuote);
-main.on('click', 'down', getQuote);
+getQuote(null);
+quotePage.show();
 
 quotePage.on('click', 'select', getQuote);
-quotePage.on('click', 'back', function(e) {
-  main.show();
-});
+
 
 // for testing
 // getQuote(null);
